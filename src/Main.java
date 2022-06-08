7eb029f65c4f68a631291471b93fc72aa04fe1e8
macbookpro <Lis.ignat@mail.ru>
2022-06-08 13:27:33 +0300

    Homework3

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+
+
+
+     Scanner sc = new Scanner(System.in);
+
+int monthNumber = sc.nextInt();
+
+if (monthNumber == 1) System.out.print("January");
+else if (monthNumber == 2) {
+        System.out.print("February");
+        }  else if (monthNumber == 3) {
+        System.out.print("March");
+        }  else if (monthNumber == 4) {
+        System.out.print("April");
+        }  else if (monthNumber == 5) {
+        System.out.print("May");
+        }  else if (monthNumber == 6) {
+        System.out.print("June");
+        }  else if (monthNumber == 7) {
+        System.out.print("July");
+        }  else if (monthNumber == 8) {
+        System.out.print("August");
+        }  else if (monthNumber == 9) {
+        System.out.print("September");
+        }  else if (monthNumber == 10) {
+        System.out.print("October");
+        }  else if (monthNumber == 11) {
+        System.out.print("November");
+        }  else if (monthNumber == 12) {
+        System.out.print("December");
+        }else;}}
