0ee2cc3a078f2e5e23370d5cf156788bc78fc18a
macbookpro <Lis.ignat@mail.ru>
2022-06-09 18:38:07 +0300

    Lesson 4 Color

--- src/Lesson 1/Lesson4.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Lesson4 {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        Scanner next = new Scanner (System.in);
-        System.out.println(next.nextLine());
-    }
-}
