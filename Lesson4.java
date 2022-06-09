be424c5559fc2b00c612182c3ef997f16977e8e8
macbookpro <Lis.ignat@mail.ru>
2022-06-09 18:38:07 +0300

    Homwork5

---
+++ Lesson4.java
@@ -0,0 +1,18 @@
+package  Lesson4 ;
+
+import java.util.Scanner;
+
+public class Lesson4 {
+
+
+    int value[] = new int[]{5, 6, 2, 4, 6, 8};
+
+    Scanner ff = new Scanner(System.in);
+    int value1 = ff.nextInt();    static {
+
+
+        System.out.println();
+
+
+    }
+}
---
+++ src/Mail.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+
+public class Mail {
+        public static void main(String[] args) {
+                //System.out.println("Hello world!");
+
+
+                //Scanner sc = new Scanner(System.in);
+
+                //int monthNumber = sc.nextInt();
+
+                //if (monthNumber == 1) System.out.print("January");
+                //else if (monthNumber == 2) {
+                //System.out.print("February");
+                // }  else if (monthNumber == 3) {
+                // System.out.print("March");
+                // }  else if (monthNumber == 4) {
+                // System.out.print("April");
+                // }  else if (monthNumber == 5) {
+                // System.out.print("May");
+                //}  else if (monthNumber == 6) {
+                // System.out.print("June");
+                //  }  else if (monthNumber == 7) {
+                //  System.out.print("July");
+                //  }  else if (monthNumber == 8) {
+                // System.out.print("August");
+                ///}  else if (monthNumber == 9) {
+                // System.out.print("September");
+                //  }  else if (monthNumber == 10) {
+                //  System.out.print("October");
+                // }  else if (monthNumber == 11) {
+                // System.out.print("November");
+                //  }  else if (monthNumber == 12) {
+                //  System.out.print("December");
+                //}else;}}
+                //int i = 5;
+                // for (; i > 0 ; i-=1){
+
+                //System.out.println(i);
+
+                // int j = 7;
+                // while (j< 101){
+                //.out.println(j);
+                //j+=7;
+
+                // int x = 0;
+                //do ()
+                //while()
+                //x++;
+                // System.out.println(x);
+
+
+                //Scanner num = new Scanner(System.in);{
+                //long gun = num.nextInt();
+                //System.out.println(num);
+                //if (gun == 1-10000000);
+                //System.out.println(gun +1);
+
+
+                //Scanner month = new Scanner(System.in);
+
+                //int d = month.nextInt();
+                //if (d == 1 - 2) {
+                  //      System.out.println("Winter");
+                //} else if (d == 3,4, 5){
+                  //      System.out.println("Spring");
+                //} else if (d == 6 - 8) {
+
+                  //      System.out.println("Summer");
+                //} else if (d == 9 - 11) {
+                  //      System.out.println("Autumm");
+                //} else ();
+                //}}}
+
+
+                //switch (=)
+
+                //case {
+
+         //Scanner df = new Scanner(System.in);
+
+         //int color = df.nextInt();
+
+          //if (color == 1) System.out.print("Red");
+          //else if (color == 2) {
+          //System.out.print("Orange");
+           //}  else if (color == 3) {
+            //System.out.print("Yealloy");
+           //}  else if (color == 4) {
+           //System.out.print("Green");
+           //}  else if (color == 5) {
+           // System.out.print("Bly");
+           //}  else if (color == 6) {
+           //  System.out.print("Violet");
+         //    }
+        //}}
+    Scanner t = new Scanner(System.in);
+
+    int num = t.nextInt();
+
+          if (num == 1) System.out.print("Red");
+                  else if (num == 2) {
+                  System.out.print("Orange");
+                  }  else if (num == 3) {
+                  System.out.print("Yealloy");
+                  }  else if (num == 4) {
+                  System.out.print("Green");
+                  }  else if (num == 5) {
+                  System.out.print("Bly");
+                  }  else if (num == 6) {
+                  System.out.print("Violet");
+                  }
+                  }}
