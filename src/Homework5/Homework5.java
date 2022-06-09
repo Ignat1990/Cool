ffb53df389a12efd6ae859ea127ed95ddd10fd2a
macbookpro <Lis.ignat@mail.ru>
2022-06-09 18:38:07 +0300

    Homwork5

---
+++ src/Homework5/Homework5.java
@@ -0,0 +1,14 @@
+package Homework5;
+
+import java.util.Scanner;
+
+public class Homework5 {
+
+    Scanner sc = new Scanner(System.in);
+    int value = sc.nextInt();{
+
+    int[] array = {2, 3, 4, 5, 6, 7};
+    for (int x: array) {
+        if (x == value) {
+        System.out.print(value);
+        } }}}
