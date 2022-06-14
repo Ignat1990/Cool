9954880ac96e9586f9d2693b83e925f3c413bea4
macbookpro <Lis.ignat@mail.ru>
2022-06-14 21:33:34 +0300

    Homerwork 7

---
+++ Homerwork5.java
@@ -0,0 +1,34 @@
+package Lesson4;
+
+
+import java.util.Arrays;
+
+
+public class Homerwork5 {
+    public static void main(String[] args) {
+        int mas[] = new int[]{11, 3, 14, 16, 7};
+
+        boolean isSorted = false;
+
+        int buf;
+
+        while (!isSorted) {
+            isSorted = true;
+            int i;
+            for (i = 0; i < mas.length - 1; i++) {
+
+                if (mas[i] > mas[i + 1]) {
+                    isSorted = false;
+                    buf = mas[i];
+                    mas[i] = mas[i + 1];
+                    mas[i + 1] = buf;
+                }
+            }
+
+            System.out.println(Arrays.toString(mas));
+
+        }
+    }
+}
+
+
--- src/Homework6.java
+++ src/Homework6.java
@@ -1,6 +1,4 @@
-import java.util.Scanner;
-
-public class Main {
+public class Homework6 {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
 
